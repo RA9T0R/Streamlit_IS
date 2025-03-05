511dbf1822899e47a99213d261fcb10f80eeef98
RA9T0R <Phongphatbangkha@gmail.com>
2025-03-05 20:37:27 +0700

    aaaa

--- custom_pages/ml_description.py
+++ custom_pages/ml_description.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("รายละเอียดของ Model การจำแนกประเภทของรถยนต์ (Vehicle type)")
